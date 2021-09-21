e09384ad8b4fe61ce9cc2d58b6b12c7f6ed296ae
KULDIP PIPALIYA <kuldipem@gmail.com>
2021-09-22 01:31:00 +0530

    new release

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export * from "./time";
-export * from "./locales";
-export * from "./countries";
+import { Timezone, timezones } from './time';
+import { Locale, locales } from './locales';
+import { CallingCode, Country, callingCodes, countries } from './countries';
+
+export {
+    timezones,
+    Timezone,
+    Locale,
+    locales,
+    CallingCode,
+    callingCodes,
+    Country,
+    countries,
+}
